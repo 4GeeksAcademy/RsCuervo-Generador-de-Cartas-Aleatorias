bc2c03cdfe88cacb708b0e2e500c675a9e6a98c1
Richard Cuervo <richardcuer@hotmail.com>
2025-02-23 04:23:55 +0000

    [Genere cartas aleatorias al clickear un Boton] me funciono con addEventListener "click", antes probe con llamar la funcion en el html con onclick pero no funcionaba me daba error.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function cartaRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function cartaAleatoria() {
+  const palos = ['♦', '♥', '♠', '♣'];
+  const valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'Q', 'J', 'A'];
+
+  const paloPosicion = cartaRandom(palos);
+  const valorCenter = cartaRandom(valores);
+
+  console.log(paloPosicion, valorCenter);
+
+  const paloStart = document.getElementById("palo-start");
+  const paloEnd = document.getElementById("palo-end");
+  const valorCenterElement = document.getElementById("valor-center");
+
+  paloStart.innerText = paloPosicion;
+  paloEnd.innerText = paloPosicion;
+  valorCenterElement.innerText = valorCenter;
+
+  if (paloPosicion === "♦" || paloPosicion === "♥") {
+    paloStart.style.color = "red";
+    paloEnd.style.color = "red";
+  } else {
+    paloStart.style.color = "black";
+    paloEnd.style.color = "black";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const boton = document.getElementById("generar-carta");
+  boton.addEventListener("click", cartaAleatoria);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
